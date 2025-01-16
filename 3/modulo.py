140d49412da0afc5749689ebccb4c3988e97b7d6
ASHKO <amirshkolnik1@gmail.com>
2025-01-16 21:48:56 +0100

    modulo - remove the number variable

--- 3/modulo.py
+++ 3/modulo.py
@@ -1,7 +1,6 @@
 number_to_check = int(input("What is the number you want to check? "))
-number = number_to_check % 2
 
-if number == 0:
+if number_to_check % 2 == 0:
     print("Even")
 else:
     print("Odd")
