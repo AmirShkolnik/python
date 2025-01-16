50f82d977fa6ca7f66eca7078ae986132c5e440d
ASHKO <amirshkolnik1@gmail.com>
2025-01-16 22:18:01 +0100

    nested - remove elif and add else

--- 3/nested.py
+++ 3/nested.py
@@ -5,13 +5,11 @@ age = int(input("What is your age? "))
 if height >= 120:
     print("You can ride the rollercoaster")
     print(age)
-    if age < 12:
+    if age <= 12:
         print("Your ticket cost $5.")
-    elif age >= 12:
-        print("Your ticket cost $7.")
     elif age <= 18:
         print("Your ticket cost $7.")
-    elif age > 18:
+    else:
         print("Your ticket cost $12.")
 else:
     print("Sorry you have to grow taller before you can ride.")
