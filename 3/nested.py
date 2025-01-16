038cc02c202677e265296db982b95514a9572415
ASHKO <amirshkolnik1@gmail.com>
2025-01-16 22:10:35 +0100

    create - nested file

---
+++ 3/nested.py
@@ -0,0 +1,13 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+age = int(input("What is your age? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    print(age)
+    if age <= 18:
+        print("Your ticket cost $7.")
+    else:
+        print("Your ticket cost $12.")
+else:
+    print("Sorry you have to grow taller before you can ride.")
