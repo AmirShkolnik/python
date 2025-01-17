1d0daa3cd1d2a9c439d611707112cefeb4da9ae7
ASHKO <amirshkolnik1@gmail.com>
2025-01-17 15:22:10 +0100

    add order pizza if statments

--- 3/order_pizza.py
+++ 3/order_pizza.py
@@ -2,3 +2,16 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+if size == "S":
+    bill = 15
+if size == "M":
+    bill = 20
+if size == "L":
+    bill = 25
+    if pepperoni == "Y":
+        bill += 3
+    if extra_cheese == "Y":
+        bill += 1
+else:
+    print(f"Your final bill is: ${bill}")
