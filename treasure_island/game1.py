d6df25d4fad41dcec0c3ef8fdd4b440246d489f9
ASHKO <amirshkolnik1@gmail.com>
2025-01-19 12:33:00 +0100

    treasure island - add if condition for direction

--- treasure_island/game1.py
+++ treasure_island/game1.py
@@ -21,3 +21,9 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.\nYour mission is to find the treasure.\nYou're at a cross road.\nWhere do you want to go?\n")
 direction = input("Type left or right\n")
 
+if direction == "left":
+    left = input("Type Swim or Wait\n")
+    
+
+else:
+    print("Fall into a hole.\nGame Over.")
