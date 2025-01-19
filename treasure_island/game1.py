750d94d81f3df47baf375c63695242b7c50c80f5
ASHKO <amirshkolnik1@gmail.com>
2025-01-19 12:31:46 +0100

    treasure island - add direction variable

--- treasure_island/game1.py
+++ treasure_island/game1.py
@@ -19,3 +19,5 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/
 *******************************************************************************''')
 print("Welcome to Treasure Island.\nYour mission is to find the treasure.\nYou're at a cross road.\nWhere do you want to go?\n")
+direction = input("Type left or right\n")
+
