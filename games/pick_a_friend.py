20fb7e1fbd767801c17e5b0c8bae1fd09362145e
ASHKO <amirshkolnik1@gmail.com>
2025-01-21 09:55:25 +0100

    pick_a_friend - add random index

--- games/pick_a_friend.py
+++ games/pick_a_friend.py
@@ -3,6 +3,9 @@ import random
 
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
 
-random_friend = random.choice(friends)
+random_friend = random.choice(friends) # Select a random string directly from the list
 
 print(random_friend)
+
+random_index = random.randint(0, 4)
+print(friends[random_index])
