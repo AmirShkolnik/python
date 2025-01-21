4b4e6e83f3bd71ccdf45bb3c984fbe9d3760fd05
ASHKO <amirshkolnik1@gmail.com>
2025-01-21 11:31:19 +0100

    rock_paper_scissors - modify user_choice to integer

--- games/rock_paper_scissors.py
+++ games/rock_paper_scissors.py
@@ -27,10 +27,7 @@ scissors = '''
 '''
 rock_paper_scissors_symbols = [rock, paper, scissors]
 selected_symbol = random.choice(rock_paper_scissors_symbols)
-user_choice = input('Type 0 for rock, 1 for paper or 2 for scissors:\n').lower()
-valid_choice = = [0 rock, paper, scissors]
-
-
+user_choice = int(input('Type 0 for rock, 1 for paper or 2 for scissors:\n'))
 
 print(f'You chose:\n{rock_paper_scissors_symbols[user_choice]}')
 
