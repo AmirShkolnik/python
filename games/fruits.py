caf9e66710cc56af1eda6c1027e68caee0654ab3
ASHKO <amirshkolnik1@gmail.com>
2025-01-21 10:16:34 +0100

    add dirty dozen

--- games/fruits.py
+++ games/fruits.py
@@ -1,3 +1,5 @@
 fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
 vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
  
+dirty_dozen = [fruits, vegetables]
+ 
