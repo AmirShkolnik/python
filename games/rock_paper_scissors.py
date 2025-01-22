7ffd5061a4a1e1e7f6e47a8f585f5614ceab79ac
ASHKO <amirshkolnik1@gmail.com>
2025-01-22 09:49:48 +0100

    rock paper scissors - comment out all strings

--- games/rock_paper_scissors.py
+++ games/rock_paper_scissors.py
@@ -25,13 +25,24 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-rock_paper_scissors_symbols = [rock, paper, scissors]
-computer_choice = random.choice(rock_paper_scissors_symbols)
-user_choice = int(input('Type 0 for rock, 1 for paper or 2 for scissors:\n'))
-print(f'You chose:\n{rock_paper_scissors_symbols[user_choice]}')
-print(f'PC chose:\n{computer_choice}')
-    
-if computer_choice > user_choice:
-    print("Computer wins")
-else:
-    print("You win")
+
+
+
+
+
+
+
+
+
+
+# rock_paper_scissors_symbols = [rock, paper, scissors]
+# computer_choice_symbols = random.choice(rock_paper_scissors_symbols)
+# user_choice = int(input('Type 0 for rock, 1 for paper or 2 for scissors:\n'))
+# print(f'You chose:\n{rock_paper_scissors_symbols[user_choice]}')
+# print(f'PC chose:\n{rock_paper_scissors_symbols[computer_choice_symbols]}')
+# computer_choice = rock_paper_scissors_symbols[computer_choice_symbols]
+
+# if computer_choice > user_choice:
+#     print("Computer wins!")
+# else:
+#    print("You win")
