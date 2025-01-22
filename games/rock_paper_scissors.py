75698258320a330ce606602ceeed21ca5a308d16
ASHKO <amirshkolnik1@gmail.com>
2025-01-22 10:32:57 +0100

    rock paper scissors -remove empty spaces

--- games/rock_paper_scissors.py
+++ games/rock_paper_scissors.py
@@ -41,7 +41,6 @@ if user_choice in [0, 1, 2]:
     elif (user_choice == 0 and computer_choice == 2) or \
     (user_choice == 1 and computer_choice == 0) or \
     (user_choice == 2 and computer_choice == 1):
-        
         print("You Win!")
     else:
         print("You Lose!")
